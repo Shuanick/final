ba0e0123fda1ff11f2dd50480f9e0d91f25c94f7
Nick <linshuan880727@gmail.com>
2024-09-17 03:00:27 +0800

    remove bcryptjs

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -8,21 +6,10 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-userSchema.pre('save', function(next) {
-  if (!this.isModified('password')) return next();
-  
-  bcrypt.hash(this.password, saltRounds, (err, hash) => {
-    if (err) return next(err);
-    this.password = hash;
-    next();
-  });
-});
 
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+  const isMatch = candidatePassword === this.password;
+  cb(null, isMatch);
 };
 
 module.exports = mongoose.model('User', userSchema);
